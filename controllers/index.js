bfd6994e03974d73c99de85f0224f74e8008a4c8
charrles17 <PrinceCharlie1998@yahoo.com>
2016-04-09 20:03:50 +0000

    Add about route

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 // Local deps
+var about = require('./about');
 var Home = require('./home');
 var Users = require('./users');
 
 module.exports = {
+    About: about,
     Home: Home,
     Users: Users
 };
